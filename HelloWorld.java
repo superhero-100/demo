695898e232ab24732d244b011e0c946e0cf2cc7b
Tagline <tagline@iMac-Pro.local>
2025-05-19 10:15:12 +0530

    first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -11,6 +11,13 @@
 //helloworld
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("hello world");
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+        System.out.println("hello world3");
+        System.out.println("hello world4");
+        System.out.println("hello world5");
+
     }
 }
